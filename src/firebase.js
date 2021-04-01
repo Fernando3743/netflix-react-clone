4924a70a121a261711e1efaecdfbb7efafd1b113
Fernando3743 <fernando201022@hotmail.com>
2021-03-31 22:01:32 -0500

    FrontEnd Done!

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBsGPqBAOarNiuUDFZoB2Bjcedmzw0174I",
+    authDomain: "netflix-react-clone-e0645.firebaseapp.com",
+    projectId: "netflix-react-clone-e0645",
+    storageBucket: "netflix-react-clone-e0645.appspot.com",
+    messagingSenderId: "438436959251",
+    appId: "1:438436959251:web:8f45037f4233fca5ccdbe6"
+  };
